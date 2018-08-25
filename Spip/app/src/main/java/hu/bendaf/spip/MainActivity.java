3bb217690a0316f06d1c066c22a415784be8d879
bendaf <bendaf92@gmail.com>
2018-08-25 21:38:14 +0200

    Create MainActivity

--- Spip/app/src/main/java/hu/bendaf/spip/MainActivity.java
+++ Spip/app/src/main/java/hu/bendaf/spip/MainActivity.java
@@ -1,13 +1,84 @@
 package hu.bendaf.spip;
 
-import android.support.v7.app.AppCompatActivity;
+import android.arch.lifecycle.Observer;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import hu.bendaf.spip.data.GroupEntry;
+import hu.bendaf.spip.data.SpipDatabase;
+import hu.bendaf.spip.databinding.ActivityMainBinding;
+import hu.bendaf.spip.databinding.GroupListItemBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding mBinding;
+    private GroupListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        mAdapter = new GroupListAdapter();
+        mBinding.rvGroups.setAdapter(mAdapter);
+
+        SpipDatabase.getInstance(this).spipDao().getGroups().observe(this, new Observer<List<GroupEntry>>() {
+            @Override public void onChanged(@Nullable List<GroupEntry> groupEntries) {
+                mAdapter.updateGroups(groupEntries);
+            }
+        });
+//        SpipRepository.getInstance(this, AppExecutors.getInstance()).addGroup("Group 1");
+//        SpipRepository.getInstance(this, AppExecutors.getInstance()).addGroup("Group 1");
+//        SpipRepository.getInstance(this, AppExecutors.getInstance()).addGroup("Group 1");
     }
+
+    public void addGroup(View view) {
+
+    }
+
+    class GroupListAdapter extends RecyclerView.Adapter<GroupListAdapter.SimpleVH> {
+
+        private List<GroupEntry> mGroups = new ArrayList<>();
+
+        @NonNull @Override
+        public SimpleVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            return new SimpleVH((GroupListItemBinding) DataBindingUtil.inflate(inflater,
+                    R.layout.group_list_item, parent, false));
+        }
+
+        @Override public int getItemCount() {
+            return mGroups.size();
+        }
+
+        @Override public void onBindViewHolder(@NonNull SimpleVH holder, int position) {
+            holder.mBinding.setGroupEntity(mGroups.get(position));
+        }
+
+        public void updateGroups(List<GroupEntry> groups) {
+            this.mGroups = groups;
+            notifyDataSetChanged();
+        }
+
+        class SimpleVH extends RecyclerView.ViewHolder {
+
+            private GroupListItemBinding mBinding;
+
+            SimpleVH(final GroupListItemBinding itemBinding) {
+                super(itemBinding.getRoot());
+                mBinding = itemBinding;
+            }
+        }
+    }
+
 }
--- Spip/app/src/main/java/hu/bendaf/spip/data/SpipRepository.java
+++ Spip/app/src/main/java/hu/bendaf/spip/data/SpipRepository.java
@@ -1,13 +1,17 @@
 package hu.bendaf.spip.data;
 
+import android.content.Context;
 import android.util.Log;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import hu.bendaf.spip.AppExecutors;
 
 /**
  * Created by bendaf on 2018. 08. 25. Spip.
  */
-public class SpipRepository {
+@SuppressWarnings("WeakerAccess") public class SpipRepository {
     private static final String LOG_TAG = SpipRepository.class.getSimpleName();
     private static SpipRepository sInstance;
     private final SpipDao mSpipDao;
@@ -18,12 +22,25 @@ public class SpipRepository {
         mExecutors = executors;
     }
 
-    public synchronized static SpipRepository getInstance(SpipDao weatherDao, AppExecutors executors) {
+    public synchronized static SpipRepository getInstance(Context context, AppExecutors executors) {
         Log.d(LOG_TAG, "Getting the repository");
         if(sInstance == null) {
-            sInstance = new SpipRepository(weatherDao, executors);
+            sInstance = new SpipRepository(SpipDatabase.getInstance(context).spipDao(), executors);
             Log.d(LOG_TAG, "Made new repository");
         }
         return sInstance;
     }
+
+    public void addGroup(final String name, final String mainCurrency, final String description, final Date date) {
+        mExecutors.diskIO().execute(new Runnable() {
+            @Override public void run() {
+                mSpipDao.addGroup(new GroupEntry(0, name, description, date, mainCurrency));
+            }
+        });
+    }
+
+    public void addGroup(String name) {
+        addGroup(name, null, null, Calendar.getInstance().getTime());
+    }
+
 }
