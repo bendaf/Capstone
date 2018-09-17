07d055d41390ec6ec2a510c45410f97583aa442e
bendaf <bendaf92@gmail.com>
2018-09-17 20:51:54 +0200

    Add AddExpenseActivity

---
+++ Spip/app/src/main/java/hu/bendaf/spip/AddExpenseActivity.java
@@ -0,0 +1,20 @@
+package hu.bendaf.spip;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import java.util.Objects;
+
+public class AddExpenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_expense);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+}
